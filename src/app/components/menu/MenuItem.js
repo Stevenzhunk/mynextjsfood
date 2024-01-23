d935923001a12d6ddd0c940cde130c37ad894ada
Stevenzhunk <kikexgaz@gmail.com>
2024-01-23 18:20:21 -0300

    menuItem->AddToCart

--- src/app/components/menu/MenuItem.js
+++ src/app/components/menu/MenuItem.js
@@ -16,61 +16,125 @@ export default function MenuItem(menuItem) {
   } = menuItem;
 
   const [showPopup, setShowPopup] = useState(false);
+  const [selectedSize, setSelectedSize] = useState(sizes?.[0] || null);
+  const [selectedExtra, setSelectedExtra] = useState([]);
+
   const { addToCart } = useContext(CartContext);
 
   const NumberBasePrice = parseInt(basePrice);
 
   function handleAddToCartButtonClick() {
-    if (sizes.length === 0 && extraIngredientPrices.length === 0) {
-      addToCart(menuItem);
-      toast.success('Added to cart!');
-    } else {
+    const hasOptions = sizes.length > 0 || extraIngredientPrices.length > 0;
+    if (hasOptions && !showPopup) {
       setShowPopup(true);
+      return;
+    }
+    addToCart(menuItem, selectedSize, selectedExtra);
+    setShowPopup(false);
+    toast.success('Added to cart!');
+  }
+
+  function handleExtraThingClick(ev, extraThing) {
+    const checked = ev.target.checked;
+    if (checked) {
+      setSelectedExtra((prev) => [...prev, extraThing]);
+    } else {
+      setSelectedExtra((prev) => {
+        return prev.filter((e) => e.name !== extraThing.name);
+      });
+    }
+  }
+
+  let selectedPrice = NumberBasePrice;
+  if (selectedSize) {
+    selectedPrice += selectedSize.price;
+  }
+  if (selectedExtra?.length > 0) {
+    for (const extra of selectedExtra) {
+      selectedPrice += extra.price;
     }
   }
 
   return (
     <>
       {showPopup && (
-        <div className="fixed inset-0 bg-black/80 flex items-center justify-center ">
-          <div className="bg-white p-4 rounded-gl max-w-md">
-            <Image
-              src={image}
-              alt={name}
-              width={200}
-              height={200}
-              className="mx-auto"
-            />
-            <h2 className="text-lg font-bold text-center mb-2">{name}</h2>
-            <p className="text-center text-gray-500 text-sm mb-2">
-              {description}
-            </p>
-            {sizes?.length > 0 && (
-              <div className="p-2">
-                <h3 className="text-center text-gray-700">Pick your size</h3>
-                {sizes.map((size) => (
-                  // eslint-disable-next-line react/jsx-key
-                  <label className="flex items-center gap-2 p-3 border rounded-md mb-1">
-                    <input type="radio" name="size" />
-                    {size.name} ${size.price + NumberBasePrice}
-                  </label>
-                ))}
-              </div>
-            )}
-            {extraIngredientPrices?.length > 0 && (
-              <div className="py-2">
+        <div
+          onClick={() => setShowPopup(false)}
+          className="fixed inset-0 bg-black/80 flex items-center justify-center "
+        >
+          <div
+            onClick={(ev) => ev.stopPropagation()}
+            className="my-8 bg-white p-4 rounded-gl max-w-md"
+          >
+            <div
+              className="overflow-y-scroll p-2"
+              style={{ maxHeight: 'calc(100vh - 100px)' }}
+            >
+              <Image
+                src={image}
+                alt={name}
+                width={200}
+                height={200}
+                className="mx-auto"
+              />
+              <h2 className="text-lg font-bold text-center mb-2">{name}</h2>
+              <p className="text-center text-gray-500 text-sm mb-2">
+                {description}
+              </p>
+              {sizes?.length > 0 && (
                 <div className="p-2">
-                  <h3 className="text-center text-gray-700">Pick your extra</h3>
-                  {extraIngredientPrices.map((extraThing) => (
-                    // eslint-disable-next-line react/jsx-key
-                    <label className="flex items-center gap-2 p-3 border rounded-md mb-1">
-                      <input type="checkbox" name={extraThing.name} />
-                      {extraThing.name} ${extraThing.price}
+                  <h3 className="text-center text-gray-700">Select Sizes</h3>
+
+                  {sizes.map((size) => (
+                    <label
+                      key={size._id}
+                      className="flex items-center gap-2 p-3 border rounded-md mb-1"
+                    >
+                      <input
+                        type="radio"
+                        onClick={() => setSelectedSize(size)}
+                        checked={selectedSize?.name === size.name}
+                        name="size"
+                      />
+                      {size.name} ${size.price + NumberBasePrice}
                     </label>
                   ))}
                 </div>
-              </div>
-            )}
+              )}
+
+              {extraIngredientPrices?.length > 0 && (
+                <div className="py-2">
+                  <div className="p-2">
+                    <h3 className="text-center text-gray-700">Any Extra ?</h3>
+                    {extraIngredientPrices.map((extraThing) => (
+                      <label
+                        key={extraThing._id}
+                        className="flex items-center gap-2 p-3 border rounded-md mb-1"
+                      >
+                        <input
+                          type="checkbox"
+                          onClick={(ev) =>
+                            handleExtraThingClick(ev, extraThing)
+                          }
+                          name={extraThing.name}
+                        />
+                        {extraThing.name} ${extraThing.price}
+                      </label>
+                    ))}
+                  </div>
+                </div>
+              )}
+              <button
+                onClick={handleAddToCartButtonClick}
+                className="primary sticky bottom-2"
+                type="button"
+              >
+                Add to cart ${selectedPrice}
+              </button>
+              <button className="mt-2" onClick={() => setShowPopup(false)}>
+                Cancel
+              </button>
+            </div>
           </div>
         </div>
       )}
--- src/app/menu/page.js
+++ src/app/menu/page.js
@@ -15,7 +15,6 @@ export default function MenuPage() {
       res.json().then((menuItems) => setMenuItems(menuItems));
     });
   }, []);
-  console.log(menuItems);
   return (
     <section className="mt-8">
       {categories?.length > 0 &&
